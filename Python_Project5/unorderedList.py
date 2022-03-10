0585ae8252996565fc84be919c2b0e4b3f807638
Opfes <xopfesx@gmail.com>
2022-03-10 11:46:00 -0600

    started project 5

---
+++ Python_Project5/unorderedList.py
@@ -0,0 +1,28 @@
+class UnorderedList:
+    def __init__(self):
+        self.head = None
+        self.length = 0
+
+    def isEmpty(self):
+        return self.head == None
+
+    def add(self,item):
+        temp = Node(item)
+        temp.setNext(self.head)
+        self.head = temp
+        self.length += 1
+    
+    def size(self):
+        return self.length
+
+    def search(self,item):
+        current = self.head
+        found = False
+        while current != None and not found:
+            if current.getData() == item:
+                found = True
+            else:
+                current = current.getNext()
+
+        return found
+
---
+++ Python_Project5/unorderedListsTest.py
@@ -0,0 +1,16 @@
+# add import statement here and run this program to get the output
+
+myList = UnorderedList()
+myList.add(31)
+myList.add(77)
+myList.add(17)
+myList.append(93)
+myList.append(26)
+myList.append(54)
+myList.printList()
+print()
+print(myList.size())
+print(myList.search(17))
+myList.remove(93)
+myList.remove(22)
+myList.printList()   
